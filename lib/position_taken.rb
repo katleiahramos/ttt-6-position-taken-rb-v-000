687bb80649180b62218e8c3ea9babcf95a73df95
Katleiah <katleiahramos@gmail.com>
2018-02-28 21:35:49 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
+  if index == nil
+    return false
   if board[index] == " " || board[index] == ""
     return false
   else
